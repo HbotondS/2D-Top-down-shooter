23d23026183265e0b2732a735a9ab8636a00e686
feketeszili <32087212+feketeszili@users.noreply.github.com>
2019-11-10 18:36:58 +0200

    look at this

--- src/2D-Top-down-shooter.cpp
+++ src/2D-Top-down-shooter.cpp
@@ -1,18 +1,34 @@
 #include <GL/freeglut.h>
 #include "GameObject.h"
+#include <iostream>
+
+GameObject gameObject(0.5, 0.5);
 
 void displayMe(void) {
-	GameObject gameObject(0.5, 0.5);
+
 	gameObject.draw();
+	
 }
+//I've also tried this, but didn't work so ff at 20 min 
+/*
+void keyboard(unsigned char key, int x, int y) {
+	 gameObject.keyboard_down(key,x,y);
+}*/
 
 int main(int argc, char** argv) {
+	unsigned char key;
+	int x = 0, y = 0;
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE);
 	glutInitWindowSize(300, 300);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("Hello world :D");
 	glutDisplayFunc(displayMe);
+	//glutKeyboardFunc(gameObject.keyboard_down(key,x,y));
+	glutKeyboardFunc(gameObject.keyboard_down);
 	glutMainLoop();
 	return 0;
+
 }
+
+
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,6 +1,8 @@
 #include "GameObject.h"
 #include <iostream>
 
+
+
 GameObject::GameObject(float width, float height): width(width), height(height) {
 	x = 0.0;
 	y = 0.0;
@@ -17,3 +19,39 @@ void GameObject::draw() {
 	glEnd();
 	glFlush();
 }
+
+void GameObject::keyboard_down(unsigned char key, int xmouse, int ymouse) 
+{
+	switch (key)
+	{
+	case 033: // octal equivalent of the Escape key
+		glutLeaveMainLoop();
+		break;
+	case 'w':
+		// increase the camera's Z velocity
+		x++;
+		std::cout << x << std::endl;
+		glutPostRedisplay();
+		break;
+	case 's':
+		// decrease the camera's Z velocity
+		x--;
+		std::cout << x << std::endl;
+		glutPostRedisplay();
+		break;
+	case 'a':
+		// decrease the camera's X velocity
+		y--;
+		std::cout << y << std::endl;
+		glutPostRedisplay();
+		break;
+	case 'd':
+		// increase the camera's X velocity
+		y++;
+		std::cout << y << std::endl;
+		glutPostRedisplay();
+		break;
+	}
+}
+
+
