b87604739514133decfe7c899262621a2b5df133
kati1989 <56889441+kati1989@users.noreply.github.com>
2019-12-13 16:37:17 +0200

    New Game Menu fix

--- src/MenuObject.cpp
+++ src/MenuObject.cpp
@@ -1,10 +1,5 @@
 #include "MenuObject.h"
-#include "Sprite.h"
 #include <iostream>
-
-Sprite* newGame;
-Sprite* settings;
-Sprite* exitGame;
 
 
 void RenderString(float x, float y, void* font, float r, float g, float b, const unsigned char* string)
@@ -16,27 +11,27 @@ void RenderString(float x, float y, void* font, float r, float g, float b, const
 }
 
 MenuObject::MenuObject() {
-	char* filenameNewGame = _strdup("res/player2_3.bmp");
-	char* filenameSettings = _strdup("res/settings.bmp");
-	char* filenameExit = _strdup("res/player2_3.bmp");
 
-	newGame = new Sprite(width, height, filenameNewGame);
-	settings = new Sprite(width, height, filenameSettings);
-	exitGame =  new Sprite(width, height, filenameExit);
 }
 
 
-void MenuObject::draw_menu_item(int offset, Sprite* sprite)
+
+
+
+void MenuObject::draw_menu_item(const unsigned char* title, float offset)
 {
-	//
-	glLoadIdentity();
-	glPopMatrix();
+	glColor3f(255, 0, 0);
 
-	glTranslatef(x, y + height + offset, 0);
-	sprite->draw();
-	glPopMatrix();
+	glBegin(GL_POLYGON);
+
+	glVertex2f(x, y - offset);
+	glVertex2f(x + width, y - offset);
+	glVertex2f(x + width, y + height - offset);
+	glVertex2f(x, y + height - offset);
+	glEnd();
+	RenderString(x + (width / 4), y - offset + (height / 3), GLUT_BITMAP_TIMES_ROMAN_24, 255, 255, 255, title);
+
 	glFlush();
-
 }
 
 void MenuObject::draw() {
@@ -44,32 +39,24 @@ void MenuObject::draw() {
 	glEnable(GL_STENCIL_TEST);
 	glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
 
-
 	unsigned char* tmpBuffer = &titleNewGame[0];
 
 	float offset = 0;
-	
 
-	glStencilFunc(GL_ALWAYS,  1, -1);
-	//draw_menu_item(tmpBuffer,offset); //kirajzol egy darab menu pontot
-	draw_menu_item(offset,newGame);
-	//newGame->draw();
+	glStencilFunc(GL_ALWAYS, 1, -1);
+	draw_menu_item(tmpBuffer, offset); //kirajzol egy darab menu pontot
 
 	offset = height + space;
 
 	tmpBuffer = &titleSettings[0];
 	glStencilFunc(GL_ALWAYS, 2, -1);
-	draw_menu_item(offset, settings);
-	//draw_menu_item(tmpBuffer, offset); //kirajzol egy darab menu pontot
-	//settings->draw();
+	draw_menu_item(tmpBuffer, offset); //kirajzol egy darab menu pontot
 
 	offset += height + space;
 
 	tmpBuffer = &titleExit[0];
 	glStencilFunc(GL_ALWAYS, 3, -1);
-	draw_menu_item(offset, exitGame);
-	//exitGame->draw();
-	//draw_menu_item(tmpBuffer, offset); //kirajzol egy darab menu pontot
+	draw_menu_item(tmpBuffer, offset); //kirajzol egy darab menu pontot
 }
 
 
--- src/MenuObject.h
+++ src/MenuObject.h
@@ -2,20 +2,19 @@
 
 #include "IDrawable.h"
 #include <GL/freeglut.h>
-#include "Sprite.h"
 
 class MenuObject : public IDrawable
 {
-private: 
-	 float x = -0.4, y = 0.3, space= 0.2;
-	 float width = 0.6, height = 0.2;
-	 void draw_menu_item(int offset, Sprite* sprite);
+private:
+	float x = -0.4, y = 0.3, space = 0.2;
+	float width = 0.6, height = 0.2;
+	void draw_menu_item(const unsigned char* title, float offset);
 	unsigned char titleNewGame[9] = "New Game";
-	 unsigned char titleSettings[9] = "Settings";
-	 unsigned char titleExit[5] = "Exit";
+	unsigned char titleSettings[9] = "Settings";
+	unsigned char titleExit[5] = "Exit";
 
-	 static unsigned char selectedItem; //0- None 1 - New Game, 2- Settings, 3 - Exit
-	
+	static unsigned char selectedItem; //0- None 1 - New Game, 2- Settings, 3 - Exit
+
 
 
 public:
